bd67e91c05113641c4c0e49d52b1ca0f984fd3d2
kanikaverma <vermakanika@hotmail.com>
2014-06-30 14:14:28 -0700

    Added Bart's changes to ParseFile()

--- GalaxySim/GalaxySim.cpp
+++ GalaxySim/GalaxySim.cpp
@@ -161,202 +161,8 @@ void InitApp();
 void RenderText();
 //declare WriteAttributes function here, content is below ParseFile()
 HRESULT WriteAttributes(IXmlReader* pReader);
+int ParseFile();
 
-int ParseFile(){
-	HRESULT hr = S_OK;
-	IStream *pFileStream = NULL;
-	IXmlReader *pReader = NULL;
-	XmlNodeType nodeType;
-	const WCHAR* pwszPrefix;
-	const WCHAR* pwszLocalName;
-	const WCHAR* pwszValue;
-	UINT cwchPrefix;
-
-	if (2 != 2) //??? Currently this is where the program is breaking. Never gets past this. argc value is 1? 
-	{
-		wprintf(L"Usage: XmlLiteReader.exe name-of-input-file\n");
-		return 0;
-	}
-
-	//Open read-only input stream 
-	LPCWSTR fileName = L"C:\\Users\\t-kaver\\Source\\Repos\\Galaxy-Simulator\\GalaxySim\\XMLFileFormatTEST.xml";
-	if (FAILED(hr = SHCreateStreamOnFile(fileName, STGM_READ, &pFileStream)))
-	{
-		wprintf(L"Error creating file reader, error is %08.8lx", hr);
-		HR(hr);
-	}
-
-	if (FAILED(hr = CreateXmlReader(__uuidof(IXmlReader), (void**)&pReader, NULL)))
-	{
-		wprintf(L"Error creating xml reader, error is %08.8lx", hr);
-		HR(hr);
-	}
-
-	if (FAILED(hr = pReader->SetProperty(XmlReaderProperty_DtdProcessing, DtdProcessing_Prohibit)))
-	{
-		wprintf(L"Error setting XmlReaderProperty_DtdProcessing, error is %08.8lx", hr);
-		HR(hr);
-	}
-
-	if (FAILED(hr = pReader->SetInput(pFileStream)))
-	{
-		wprintf(L"Error setting input for reader, error is %08.8lx", hr);
-		HR(hr);
-	}
-
-	//read until there are no more nodes 
-	//Pretty sure this is where we would change the printf's outside the if's so they go to an array? probably? 
-	int first = 0;
-	int var = 0;
-	int count = 0;
-	while (S_OK == (hr = pReader->Read(&nodeType)))
-	{
-		switch (nodeType)
-		{
-		case XmlNodeType_XmlDeclaration:
-			wprintf(L"XmlDeclaration\n");
-			if (FAILED(hr = WriteAttributes(pReader)))
-			{
-				wprintf(L"Error writing attributes, error is %08.8lx", hr);
-				HR(hr);
-			}
-			break;
-		case XmlNodeType_Element:
-			if (FAILED(hr = pReader->GetPrefix(&pwszPrefix, &cwchPrefix)))
-			{
-				wprintf(L"Error getting prefix, error is %08.8lx", hr);
-				HR(hr);
-			}
-			if (FAILED(hr = pReader->GetLocalName(&pwszLocalName, NULL)))
-			{
-				wprintf(L"Error getting local name, error is %08.8lx", hr);
-				HR(hr);
-			}
-			if (cwchPrefix > 0){
-				wprintf(L"Element: %s:%s\n", pwszPrefix, pwszLocalName);
-			}
-			else {
-				//wprintf(L"Element: %s\n", pwszLocalName);
-				if (first == 0)
-				{
-					NUM_PARTICLES = (int)pwszLocalName;
-					name = new string[NUM_PARTICLES];
-					mass = new float[NUM_PARTICLES];
-					diameter = new float[NUM_PARTICLES];
-					brightness = new int[NUM_PARTICLES];
-					xcoord = new float[NUM_PARTICLES];
-					ycoord = new float[NUM_PARTICLES];
-					zcoord = new float[NUM_PARTICLES];
-				}
-				else
-				{
-					if (var == 0)
-					{
-						char buffer[maxnamesize];
-						wcstombs_s(NULL, buffer, sizeof(char) * maxnamesize, pwszLocalName, maxnamesize);
-						name[count] = buffer;
-					}
-					else if (var == 1)
-						mass[count] = wcstof(pwszLocalName, NULL);
-					else if (var == 2)
-						diameter[count] = wcstof(pwszLocalName, NULL);
-					else if (var == 3)
-						brightness[count] = int(pwszLocalName);
-					else if (var == 4)
-						xcoord[count] = wcstof(pwszLocalName, NULL);
-					else if (count == 5)
-						ycoord[count] = wcstof(pwszLocalName, NULL);
-					else if (var == 6)
-						zcoord[count] = wcstof(pwszLocalName, NULL);
-					var++;
-				}
-			}
-			if (FAILED(hr = WriteAttributes(pReader)))
-			{
-				wprintf(L"Error writing attributes, error is %08.8lx", hr);
-				HR(hr);
-			}
-
-			if (pReader->IsEmptyElement())
-				wprintf(L" (empty)");
-			break;
-		case XmlNodeType_EndElement:
-			if (FAILED(hr = pReader->GetPrefix(&pwszPrefix, &cwchPrefix)))
-			{
-				wprintf(L"Error getting prefix, error is %08.8lx", hr);
-				HR(hr);
-			}
-			if (FAILED(hr = pReader->GetLocalName(&pwszLocalName, NULL)))
-			{
-				wprintf(L"Error getting local name, error is %08.8lx", hr);
-				HR(hr);
-			}
-			if (cwchPrefix > 0)
-				wprintf(L"End Element: %s:%s\n", pwszPrefix, pwszLocalName);
-			else
-			{
-				wprintf(L"End Element: %s\n", pwszLocalName);
-				if (first == 0)
-				{
-					first = 1;
-				}
-				else
-				{
-					count++;
-					var = 0;
-				}
-			}
-			break;
-		case XmlNodeType_Text:
-		case XmlNodeType_Whitespace:
-			if (FAILED(hr = pReader->GetValue(&pwszValue, NULL)))
-			{
-				wprintf(L"Error getting value, error is %08.8lx", hr);
-				HR(hr);
-			}
-			wprintf(L"Text: %s\n", pwszValue);
-			break;
-		case XmlNodeType_CDATA:
-			if (FAILED(hr = pReader->GetValue(&pwszValue, NULL)))
-			{
-				wprintf(L"Error getting value, error is %08.8lx", hr);
-				HR(hr);
-			}
-			wprintf(L"CDATA: %s\n", pwszValue);
-			break;
-		case XmlNodeType_ProcessingInstruction:
-			if (FAILED(hr = pReader->GetLocalName(&pwszLocalName, NULL)))
-			{
-				wprintf(L"Error getting name, error is %08.8lx", hr);
-				HR(hr);
-			}
-			if (FAILED(hr = pReader->GetValue(&pwszValue, NULL)))
-			{
-				wprintf(L"Error getting value, error is %08.8lx", hr);
-				HR(hr);
-			}
-			wprintf(L"Processing Instruction name:%s value:%s\n", pwszLocalName, pwszValue);
-			break;
-		case XmlNodeType_Comment:
-			if (FAILED(hr = pReader->GetValue(&pwszValue, NULL)))
-			{
-				wprintf(L"Error getting value, error is %08.8lx", hr);
-				HR(hr);
-			}
-			wprintf(L"Comment: %s\n", pwszValue);
-			break;
-		case XmlNodeType_DocumentType:
-			wprintf(L"DOCTYPE is not printed\n");
-			break;
-		}
-		//printf("%d\n", g_maxparticles);
-	}
-
-CleanUp:
-	SAFE_RELEASE(pFileStream);
-	SAFE_RELEASE(pReader);
-	return hr;
-}
 //--------------------------------------------------------------------------------------
 // Entry point to the program. Initializes everything and goes into a message processing 
 // loop. Idle time is used to render the scene.
@@ -415,7 +221,239 @@ void InitApp()
 // Parse File 
 //--------------------------------------------------------------------------------------
 
+int ParseFile(){
+	HRESULT hr = S_OK;
+	IStream *pFileStream = NULL;
+	IXmlReader *pReader = NULL;
+	XmlNodeType nodeType;
+	const WCHAR* pwszPrefix;
+	const WCHAR* pwszLocalName;
+	const WCHAR* pwszValue;
+	UINT cwchPrefix;
 
+	if (2 != 2) //??? Currently this is where the program is breaking. Never gets past this. argc value is 1? 
+	{
+		wprintf(L"Usage: XmlLiteReader.exe name-of-input-file\n");
+		return 0;
+	}
+
+	//Open read-only input stream 
+	LPCWSTR fileName = L"C:\\Users\\t-kaver\\Source\\Repos\\Galaxy-Simulator\\GalaxySim\\XMLFileFormatTEST.xml";
+	if (FAILED(hr = SHCreateStreamOnFile(fileName, STGM_READ, &pFileStream)))
+	{
+		wprintf(L"Error creating file reader, error is %08.8lx", hr);
+		HR(hr);
+	}
+
+	if (FAILED(hr = CreateXmlReader(__uuidof(IXmlReader), (void**)&pReader, NULL)))
+	{
+		wprintf(L"Error creating xml reader, error is %08.8lx", hr);
+		HR(hr);
+	}
+
+	if (FAILED(hr = pReader->SetProperty(XmlReaderProperty_DtdProcessing, DtdProcessing_Prohibit)))
+	{
+		wprintf(L"Error setting XmlReaderProperty_DtdProcessing, error is %08.8lx", hr);
+		HR(hr);
+	}
+
+	if (FAILED(hr = pReader->SetInput(pFileStream)))
+	{
+		wprintf(L"Error setting input for reader, error is %08.8lx", hr);
+		HR(hr);
+	}
+
+	//read until there are no more nodes 
+	//Pretty sure this is where we would change the printf's outside the if's so they go to an array? probably? 
+	int first = 0;
+	int var = 0;
+	int count = 0;
+
+	const WCHAR * elementName = NULL;
+
+	while (S_OK == (hr = pReader->Read(&nodeType)))
+	{
+		switch (nodeType)
+		{
+		case XmlNodeType_XmlDeclaration:
+			wprintf(L"XmlDeclaration\n");
+			if (FAILED(hr = WriteAttributes(pReader)))
+			{
+				wprintf(L"Error writing attributes, error is %08.8lx", hr);
+				HR(hr);
+			}
+			break;
+		case XmlNodeType_Element:
+			if (FAILED(hr = pReader->GetPrefix(&pwszPrefix, &cwchPrefix)))
+			{
+				wprintf(L"Error getting prefix, error is %08.8lx", hr);
+				HR(hr);
+			}
+			if (FAILED(hr = pReader->GetLocalName(&pwszLocalName, NULL)))
+			{
+				wprintf(L"Error getting local name, error is %08.8lx", hr);
+				HR(hr);
+			}
+			if (cwchPrefix > 0){
+				wprintf(L"Element: %s:%s\n", pwszPrefix, pwszLocalName);
+			}
+			else {
+
+				elementName = pwszLocalName;
+
+#if 0
+				//wprintf(L"Element: %s\n", pwszLocalName);
+				if (first == 0)
+				{
+					NUM_PARTICLES = (int)pwszLocalName;
+					name = new string[NUM_PARTICLES];
+					mass = new float[NUM_PARTICLES];
+					diameter = new float[NUM_PARTICLES];
+					brightness = new int[NUM_PARTICLES];
+					xcoord = new float[NUM_PARTICLES];
+					ycoord = new float[NUM_PARTICLES];
+					zcoord = new float[NUM_PARTICLES];
+				}
+				else
+				{
+					if (var == 0)
+					{
+						char buffer[maxnamesize];
+						wcstombs_s(NULL, buffer, sizeof(char) * maxnamesize, pwszLocalName, maxnamesize);
+						name[count] = buffer;
+					}
+					else if (var == 1)
+						mass[count] = wcstof(pwszLocalName, NULL);
+					else if (var == 2)
+						diameter[count] = wcstof(pwszLocalName, NULL);
+					else if (var == 3)
+						brightness[count] = int(pwszLocalName);
+					else if (var == 4)
+						xcoord[count] = wcstof(pwszLocalName, NULL);
+					else if (count == 5)
+						ycoord[count] = wcstof(pwszLocalName, NULL);
+					else if (var == 6)
+						zcoord[count] = wcstof(pwszLocalName, NULL);
+					var++;
+				}
+#endif
+
+			}
+			if (FAILED(hr = WriteAttributes(pReader)))
+			{
+				wprintf(L"Error writing attributes, error is %08.8lx", hr);
+				HR(hr);
+			}
+
+			if (pReader->IsEmptyElement())
+				wprintf(L" (empty)");
+			break;
+		case XmlNodeType_EndElement:
+			if (FAILED(hr = pReader->GetPrefix(&pwszPrefix, &cwchPrefix)))
+			{
+				wprintf(L"Error getting prefix, error is %08.8lx", hr);
+				HR(hr);
+			}
+			if (FAILED(hr = pReader->GetLocalName(&pwszLocalName, NULL)))
+			{
+				wprintf(L"Error getting local name, error is %08.8lx", hr);
+				HR(hr);
+			}
+			if (cwchPrefix > 0)
+				wprintf(L"End Element: %s:%s\n", pwszPrefix, pwszLocalName);
+			else
+			{
+				//wprintf(L"End Element: %s\n", pwszLocalName);
+				if (first == 0)
+				{
+					first = 1;
+				}
+				else
+				{
+					count++;
+					var = 0;
+				}
+			}
+			break;
+
+		case XmlNodeType_Text:
+			if (FAILED(hr = pReader->GetValue(&pwszValue, NULL)))
+			{
+				wprintf(L"Error getting value, error is %08.8lx", hr);
+				HR(hr);
+			}
+			wprintf(L"Text: %s\n", pwszValue);
+
+			if (elementName != NULL && wcscmp(elementName, L"MaxParticles") == 0)
+			{
+				NUM_PARTICLES = _wtoi(pwszValue);
+
+				name = new string[NUM_PARTICLES];
+				mass = new float[NUM_PARTICLES];
+				diameter = new float[NUM_PARTICLES];
+				brightness = new int[NUM_PARTICLES];
+				xcoord = new float[NUM_PARTICLES];
+				ycoord = new float[NUM_PARTICLES];
+				zcoord = new float[NUM_PARTICLES];
+			}
+
+			if (elementName != NULL && wcscmp(elementName, L"object") == 0){
+
+			}
+
+
+			break;
+
+		case XmlNodeType_Whitespace:
+			if (FAILED(hr = pReader->GetValue(&pwszValue, NULL)))
+			{
+				wprintf(L"Error getting value, error is %08.8lx", hr);
+				HR(hr);
+			}
+			wprintf(L"Text: %s\n", pwszValue);
+			break;
+
+		case XmlNodeType_CDATA:
+			if (FAILED(hr = pReader->GetValue(&pwszValue, NULL)))
+			{
+				wprintf(L"Error getting value, error is %08.8lx", hr);
+				HR(hr);
+			}
+			wprintf(L"CDATA: %s\n", pwszValue);
+			break;
+		case XmlNodeType_ProcessingInstruction:
+			if (FAILED(hr = pReader->GetLocalName(&pwszLocalName, NULL)))
+			{
+				wprintf(L"Error getting name, error is %08.8lx", hr);
+				HR(hr);
+			}
+			if (FAILED(hr = pReader->GetValue(&pwszValue, NULL)))
+			{
+				wprintf(L"Error getting value, error is %08.8lx", hr);
+				HR(hr);
+			}
+			wprintf(L"Processing Instruction name:%s value:%s\n", pwszLocalName, pwszValue);
+			break;
+		case XmlNodeType_Comment:
+			if (FAILED(hr = pReader->GetValue(&pwszValue, NULL)))
+			{
+				wprintf(L"Error getting value, error is %08.8lx", hr);
+				HR(hr);
+			}
+			wprintf(L"Comment: %s\n", pwszValue);
+			break;
+		case XmlNodeType_DocumentType:
+			wprintf(L"DOCTYPE is not printed\n");
+			break;
+		}
+		//printf("%d\n", g_maxparticles);
+	}
+
+CleanUp:
+	SAFE_RELEASE(pFileStream);
+	SAFE_RELEASE(pReader);
+	return hr;
+}
 
 HRESULT WriteAttributes(IXmlReader* pReader)
 {
