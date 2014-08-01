f918393ab5d776be98c8261a692c92197c1eb980
mloppnow <malloppnow@gmail.com>
2014-07-31 18:42:54 -0700

    Framework for copying dxdiag file to share

--- GalaxySim/GalaxySim.cpp
+++ GalaxySim/GalaxySim.cpp
@@ -39,8 +39,8 @@
 #include <string>
 #include <fstream>
 #include <iomanip>
-
-
+#include <windows.h>
+#include <Lmcons.h>
 #include "atlbase.h"
 #include "atlstr.h"
 #include "comutil.h"
@@ -217,6 +217,7 @@ double g_oneFrameTime; //collects time for one frame
 double g_elapsedTimeAt100Days;
 wofstream g_dataFile;
 const LPCWSTR g_localFileName = L"SkyXTelemetryData.csv";
+const LPCWSTR g_currentDxDiagName = L"dxdiag.txt";
 double g_elapsedTimeAt365Days;
 double g_deltatResetParticles;
 double g_deltatResetCamera;
@@ -1518,25 +1519,54 @@ void initializeFile() {
 
 
 void copyFile() {
+	//copy data csv
 	srand(time(NULL));
 	int num = rand();
 	wostringstream wss;
 	wss << L"\\\\davis\\public\\GRFXExplorerInternship\\Telemetry\\" << num << g_localFileName;
 	const wstring& wstr = wss.str();
 	const LPCWSTR copyName = wstr.c_str();
-	bool copied = CopyFileW(g_localFileName, copyName, true);
-	if (!copied) {
-		LPCTSTR failureMessage = L"The telemetry data file was not successfully copied to the share. \nPlease email the file called SkyXTelemetryData.csv\n(in the same folder as the executable) to t-mellop@microsoft.com";
-		MessageBox(NULL, failureMessage, NULL, MB_OK);
+	bool copiedCSV = CopyFileW(g_localFileName, copyName, true);
+
+	//copy dxdiag txt
+	wss.clear();
+	wss << L"\\\\davis\\public\\GRFXExplorerInternship\\Telemetry\\" << num << g_currentDxDiagName;
+	const wstring& wstrDiag = wss.str();
+	const LPCWSTR copyDiagName = wstrDiag.c_str();
+	bool copiedDxDiag = CopyFileW(g_currentDxDiagName, copyDiagName, true);
+
+
+	if (!copiedCSV && !copiedDxDiag) {
+		LPCTSTR failureMessageBoth = L"The telemetry data and DXDiag files were not successfully copied to the share. \nPlease email the files called SkyXTelemetryData.csv and dxdiag.txt\n(in the same folder as the executable) to t-mellop@microsoft.com";
+		MessageBox(NULL, failureMessageBoth, NULL, MB_OK);
+	}
+	else if (!copiedCSV && copiedDxDiag) {
+		LPCTSTR failureMessageCSV = L"The telemetry data file was not successfully copied to the share. \nPlease email the file called SkyXTelemetryData.csv\n(in the same folder as the executable) to t-mellop@microsoft.com";
+		MessageBox(NULL, failureMessageCSV, NULL, MB_OK);
+	}
+	else if (copiedCSV && !copiedDxDiag) {
+		LPCTSTR failureMessageDiag = L"The DXDiag file was not successfully copied to the share. \nPlease email the file called dxdiag.txt\n(in the same folder as the executable) to t-mellop@microsoft.com";
+		MessageBox(NULL, failureMessageDiag, NULL, MB_OK);
 	}
 	else {
-		LPCTSTR successMessage = L"The telemetry data file was successfully copied to the share! \nThank you so much for your help!";
+		LPCTSTR successMessage = L"The telemetry data and DXDiag files were successfully copied to the share! \nThank you so much for your help!";
 		LPCTSTR boxTitle = L"Upload Complete";
 		MessageBox(NULL, successMessage, boxTitle, MB_OK);
 	}
 }
 
-
+//void getDxDiag() {
+//	wchar_t username[UNLEN + 1];
+//	DWORD username_len = UNLEN + 1;
+//	GetUserNameW(username, &username_len);
+//
+//	wostringstream wss;
+//	wss << L"C:\\Users\\" << username << L"\\testing.txt"; 
+//
+//	const wstring& wstr = wss.str();
+//	const LPCWSTR dxDiagLoc = wstr.c_str();
+//
+//}
 
 //--------------------------------------------------------------------------------------
 HRESULT CreateParticlePosVeloBuffers(ID3D11Device* pd3dDevice)
@@ -2350,7 +2380,7 @@ bool RenderParticles(ID3D11DeviceContext* pd3dImmediateContext, CXMMATRIX mView,
 //--------------------------------------------------------------------------------------
 void automatedTelemetry(){
 
-	if (g_frameCounter % 100 != 0) {
+	if (g_frameCounter % 10 != 0) {
 		return;
 	}
 
@@ -2393,7 +2423,6 @@ void automatedTelemetry(){
 		g_dataFile << "1 Frame @ 100 iteration/frame" << "," << hundredIterationPerFrame << endl;
 		break;
 	}
-
 	case 5: {
 		double winToFullTime;
 		winToFullTime = getWinToFullTime();
@@ -2484,7 +2513,11 @@ void automatedTelemetry(){
 
 		g_dataFile << "Average FPS" << "," << averageFPS << endl;
 		g_dataFile << wss.str().c_str() << endl;
+
+		//getDxDiag();
+
 		g_dataFile.close();
+
 
 		copyFile();
 
